3805e76e71e955ac09ed5f8be0710f15c17d9ecf
Kobe Amoh <kobexamoh@gmail.com>
2026-03-27 16:33:43 -0600

    Initial learning area for array scratchpad

---
+++ public/arrays101.js
@@ -0,0 +1,64 @@
+// at the end of the day we're going to be working with a demo array simulating a response from a server where the objects will be various players in the Champions League all having an id, name, position, their club, and play style attribute. For kicks, we'll have copilot generate it based on the following prompt: "Generate an array of 5 objects representing football players in the Champions League. Each object should have the following properties: id, name, position, club, and playStyle."
+
+let championsLeaguePlayers = [
+    {
+        id: 1,
+        name: "Ousmane Dembélé",
+        position: "Forward",
+        club: "Paris Saint-Germain",
+        playStyle: ""
+    },
+    {
+        id: 2,
+        name: "Dominik Szoboszlai",
+        position: "Midfielder",
+        club: "Liverpool",
+        playStyle: "Dynamic and versatile playmaker"
+    },
+    {
+        id: 3,
+        name: "Vinicius Jr.",
+        position: "Forward",
+        club: "Real Madrid",
+        playStyle: "Explosive pace and dribbling skills on the wing"
+    },
+    {
+        id: 4,
+        name: "Harry Kane",
+        position: "Forward",
+        club: "Bayern Munich",
+        playStyle: "Clinical and consistent goal scorer"
+    },
+    {
+        id: 5,
+        name: "Marcus Rashford",
+        position: "Forward",
+        club: "Barcelona",
+        playStyle: "Explosive pace and clinical finishing"
+    },
+    {
+        id: 6,
+        name: "Julian Alvarez",
+        position: "Forward",
+        club: "Atletico Madrid",
+        playStyle: "Clinical and consistent goal scorer"
+    },
+    {
+        id: 7,
+        name: "Luis Suárez (no, not THAT one)",
+        position: "Forward",
+        club: "Sporting Lisbon",
+        playStyle: "Unsure."
+    },
+    {
+        id: 8,
+        name: "Gabriel Martinelli",
+        position: "Forward",
+        club: "Arsenal",
+        playStyle: "Explosive pace and clinical finishing"
+    }
+];
+
+// update: copilot sucked at this. just use your own data.
+
+// console.log(championsLeaguePlayers);
